58a43473ac55cb79b8e160c30d178e36994c79dc
amoose <amos@amosarts.com>
2014-12-17 15:44:06 -0800

    Removes overrides on full host

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,18 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
-
-
-# if Rails.env.production?
-#   module OmniAuth
-#     module Strategy
-#       def full_host
-#         uri = URI.parse(request.url)
-#         # uri.path = ''
-#         # uri.query = nil
-#         uri.port = (uri.scheme == 'https' ? 443 : 80)
-#         uri.to_s
-#       end
-#     end
-#   end
-# end
