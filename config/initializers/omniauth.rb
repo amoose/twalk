de3423f215b2d3949c88a86773bbecb37fa49e1a
amoose <amos@amosarts.com>
2014-12-17 15:12:29 -0800

    Removes full path override for path. only port

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,8 +8,8 @@ if Rails.env.production?
     module Strategy
       def full_host
         uri = URI.parse(request.url)
-        uri.path = ''
-        uri.query = nil
+        # uri.path = ''
+        # uri.query = nil
         uri.port = (uri.scheme == 'https' ? 443 : 80)
         uri.to_s
       end
