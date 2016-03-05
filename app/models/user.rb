d9564ccb8249c58b402f8fcdbe78398bff7458cb
amoose <amos@amosarts.com>
2016-03-04 19:14:11 -0800

    Updates email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   extend FriendlyId
   friendly_id :nickname, use: :slugged
 
-  validates_format_of :email, :with => /@/, :allow_blank => true
+  validates_format_of :email, :with => /.+@.+\..+/i, :allow_blank => true
 
   def self.create_with_omniauth(auth)
     create! do |user|
